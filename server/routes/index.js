69eb553d5b38170a33508f0383f5a03c230332c0
Erlie Shang <erlie.shang@uwaterloo.ca>
2017-12-05 22:24:25 -0500

    add privacy

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,9 @@ var router = express.Router();
 router.get("/", function (req, res) {
     res.render("index.html");
 });
+router.get("/privacy", function (req, res) {
+    res.render("privacypolicy.htm");
+});
+
 
 module.exports = router;
